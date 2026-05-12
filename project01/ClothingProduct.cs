5a9db6793fa3bc8637a23015d44dddad8aa75afd
Tasnim ALGhafri <tasnimalghafri1@gmail.com>
2026-05-12 09:09:52 +0400

    Added customer and product classes with inheritance

---
+++ project01/ClothingProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace OrderProcessingSystem.Models
+{
+    // Inheritance
+    public class ClothingProduct : Product
+    {
+        public ClothingProduct(
+            string id,
+            string name,
+            double price,
+            int stockQuantity)
+            : base(id, name, price, stockQuantity)
+        {
+        }
+
+        // Polymorphism
+        public override double CalculateDiscount()
+        {
+            return Price * 0.20; // 20% discount
+        }
+    }
+}
---
+++ project01/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace OrderProcessingSystem.Models
+{
+    public class Customer
+    {
+        // Encapsulation
+        private string id;
+        private string name;
+        private string email;
+
+        // Properties
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        // Orders List
+        public List<Order> Orders { get; set; }
+
+        // Constructor
+        public Customer(string id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+
+            Orders = new List<Order>();
+        }
+    }
+}
